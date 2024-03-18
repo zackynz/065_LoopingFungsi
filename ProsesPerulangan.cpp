cf8c9e9058731959b181f08cd94157506af149f9
zackynz <nurasfanz@gmail.com>
2024-03-18 14:30:14 +0700

    looping menggunakan for

--- ProsesPerulangan.cpp
+++ ProsesPerulangan.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int i;
+    int arr[5];
 
-    for(i = 0;1 < 5; i++)
+    for(i = 0; i < 5; i++)
     {
         cout << i << " : " << "Zacky" << endl; 
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan Nama = ";
+        cin >> arr[i];
+
+    }
+
+    for (i = 0; i < 5; i++) {
+        cout << "Nama ke - " << i << " : " << arr[i] << endl;
     }
 
 }
